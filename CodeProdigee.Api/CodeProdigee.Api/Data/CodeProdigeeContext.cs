fba429bdf0491ad598df488c41882f467ad2c46a
Joe Izang <joeizang@hotmail.com>
2021-05-27 22:59:12 +0100

    Add dbcontext to API project and configured startup project

---
+++ CodeProdigee.Api/CodeProdigee.Api/Data/CodeProdigeeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeProdigee.Api.Data
+{
+    public class CodeProdigeeContext : DbContext
+    {
+        public CodeProdigeeContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
