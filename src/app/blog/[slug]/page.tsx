68a551743e15af13729de8a310077a7107c0f095
Joseph Izang <josephizang@gmail.com>
2024-07-20 23:14:10 +0100

    move types to d.ts file

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,21 +1,9 @@
 import Post from '@/components/post'
-import { PostTags } from '@/index'
+import { BlogPostProps, PostTags } from '@/index'
 import React from 'react'
 
+export default function BlogPost(props: BlogPostProps) {
 
-export interface PostProps {
-  postImage?: string
-  postBody?: string
-  postAuthor?: string
-  tags?: PostTags[]
-}
-
-export interface BlogPostProps {
-  slug: string
-}
-
-export default function BlogPost (props: BlogPostProps) {
-  console.log('what is in here?', props)
   return (
     <main className="flex min-h-screen flex-col items-center mx-auto text-stone-200">
       <Post />
--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,14 @@ export interface PostTags {
   name: string
   tagId: string
 }
+
+export interface PostProps {
+  postImage?: string
+  postBody?: string
+  postAuthor?: string
+  tags?: PostTags[]
+}
+
+export interface BlogPostProps {
+  slug: string
+}
