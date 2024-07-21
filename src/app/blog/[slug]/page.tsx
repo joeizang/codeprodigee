69fafc33808b0515201ebd49c791bd4488df6da5
Joseph Izang <josephizang@gmail.com>
2024-07-21 18:55:35 +0100

    change type source.

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,8 +1,7 @@
 import Post from '@/components/post'
-import { BlogPostProps, PostTags } from '@/index'
 import React from 'react'
 
-export default function BlogPost(props: BlogPostProps) {
+export default function BlogPost(props: { slug: string }) {
 
   return (
     <main className="flex min-h-screen flex-col items-center mx-auto text-stone-200">
